7a776a259fd5e33bd01138afd1c9837e189012e8
David Klawitter <davidklaw@gmail.com>
2012-07-17 22:46:41 -0400

    Initial commit

---
+++ completer.jsx
@@ -0,0 +1,56 @@
+#target photoshop
+
+main();
+
+function findTextLayers(doc, foundLayers) 
+{
+  var layersCount = doc.layers.length;
+  for (var layersIndex = 0; layersIndex < layersCount; layersIndex++) {
+    var layerRef = doc.layers[layersIndex];
+
+    if (layerRef.typename == "ArtLayer") {
+      if (layerRef.visible && layerRef.kind == "LayerKind.TEXT") {
+        var text = layerRef.textItem;
+        foundLayers.push(layerRef);
+        //foundLayers.push(Math.round(text.size) + userFriendly(app.preferences.typeUnits) + ", " + text.font + ", #" + text.color.nearestWebColor.hexValue);
+      }
+    } else if (layerRef.typename == "LayerSet") {
+      if (layerRef.visible) {
+        findTextLayers(layerRef, foundLayers);
+      }
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Function: userFriendly
+// Usage: Converts constants to user-friendly copy
+// Input: string
+// Return: a string
+///////////////////////////////////////////////////////////////////////////
+function userFriendly(obj) 
+{
+  if (obj == "TypeUnits.PIXELS")
+    return "px";
+  else if (obj == "TypeUnits.POINTS") {
+    return "pt";
+  } else {
+    return obj;
+  }
+}
+
+function main()
+{
+  var layerCompsCount = app.activeDocument.layerComps.length;
+  for (layerCompsIndex = 0; layerCompsIndex < layerCompsCount; layerCompsIndex++) {
+    var textLayers = new Array();
+    var layerCompRef = activeDocument.layerComps[layerCompsIndex];
+
+    layerCompRef.apply();
+
+    // Collect text layers for comp
+    findTextLayers(app.activeDocument, textLayers);
+    
+    alert(textLayers);
+  }  
+}
